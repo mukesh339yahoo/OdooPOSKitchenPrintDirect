cc79fc29e68d898cb5112e6091dc151df2f474a5
mukesh339yahoo <mukeshsharma339@yahoo.com>
2026-07-22 16:48:07 +0530

    Implement dynamic proxy IP assignment for full mobile tablet support

--- ridhira_pos_kitchen_print_direct/static/src/js/pos_print_override.js
+++ ridhira_pos_kitchen_print_direct/static/src/js/pos_print_override.js
@@ -3,22 +3,8 @@
 import { HWPrinter } from "@point_of_sale/app/printer/hw_printer"; 
 import { patch } from "@web/core/utils/patch";
 
-// The base URL for your Python proxy (Keep this to ensure connection)
-const PROXY_URL = "http://localhost:9100"; 
-
-// --- 1. PATCH: HWPrinter to set URL and suppress error ---
+// --- 1. PATCH: HWPrinter to suppress error ---
 patch(HWPrinter.prototype, {
-    
-    // Override setup to replace the default IoT URL with your custom proxy URL base
-    setup(params) {
-        // Call the original setup to initialize properties
-        super.setup(...arguments);
-        
-        // **Override the URL** that HWPrinter uses for RPC calls
-        this.url = PROXY_URL;
-        
-        console.log("[Ridhira Proxy] HWPrinter URL redirected to:", this.url);
-    },
     
     /**
      * @override
@@ -43,16 +29,22 @@ patch(HWPrinter.prototype, {
 
 // --- 2. PATCH: EpsonPrinter to intercept and route to HWPrinter ---
 // In Odoo Enterprise, IoT Box selection might be restricted. This allows
-// users to configure an 'ePos Printer' with a dummy IP, and we'll secretly
+// users to configure an 'ePos Printer' with their Proxy IP, and we'll secretly
 // route that print job to our Python Proxy via the HWPrinter logic!
 import { EpsonPrinter } from "@pos_epson_printer/app/epson_printer";
 
 patch(EpsonPrinter.prototype, {
     setup(params) {
         super.setup(...arguments);
+        
+        // Dynamically build the proxy URL using the IP configured in Odoo.
+        // If they enter '192.168.1.100' as the Epson printer IP, it will target that IP on port 9100.
+        const proxyIp = params.ip || "localhost";
+        const proxyUrl = `http://${proxyIp}:9100`;
+
         // Create an internal HWPrinter instance pointing to our proxy
-        this.ridhira_proxy_printer = new HWPrinter({ url: PROXY_URL });
-        console.log("[Ridhira Proxy] EpsonPrinter intercepted. Jobs will route to:", PROXY_URL);
+        this.ridhira_proxy_printer = new HWPrinter({ url: proxyUrl });
+        console.log("[Ridhira Proxy] EpsonPrinter intercepted. Jobs will route to:", proxyUrl);
     },
 
     async printReceipt(receipt) {
