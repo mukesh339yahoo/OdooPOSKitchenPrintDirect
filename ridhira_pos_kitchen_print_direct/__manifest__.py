5c893c270a790cdba059c1ce4b9423c082f18c98
mukesh339yahoo <mukeshsharma339@yahoo.com>
2026-08-02 05:55:24 +0530

    Add support email to manifest for Odoo Marketplace

--- ridhira_pos_kitchen_print_direct/__manifest__.py
+++ ridhira_pos_kitchen_print_direct/__manifest__.py
@@ -4,6 +4,7 @@
     'summary': 'No IOT Box required. Manage POS and Kitchen Printers with Test Print feature. Supports Self Orders. Supports mobile devices. Print Job live dashboard included.',
     'description': "Integrates Odoo POS with local/network printers using a Python proxy. Supports Self Orders.",
     'author': 'Ridhira Technologies, Pune, India',
+    'support': 'ridhiratech@gmail.com',
     'website': 'https://ridhira.desigoogly.com',
     'category': 'Point of Sale',
     'depends': ['point_of_sale'],
